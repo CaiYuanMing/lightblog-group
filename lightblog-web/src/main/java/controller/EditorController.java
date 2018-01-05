e1781f54444dbfb482049c344b4ebed8107fa307
ToBeProf <cympr@outlook.com>
2018-01-05 23:15:50 +0800

    success{editPage-image upload}

--- lightblog-web/src/main/java/controller/EditorController.java
+++ lightblog-web/src/main/java/controller/EditorController.java
@@ -1,14 +1,21 @@
 package controller;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 import pojo.WorkTemp;
 import service.WorkService;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -65,6 +72,37 @@ public class EditorController {
             log.info("--再编辑文章处理:end");
         }
         return resultMap;
+    }
+
+
+    @RequestMapping("imageUpload")
+    public void imageUpload(HttpServletRequest request, HttpServletResponse response, @RequestParam(value = "editormd-image-file", required = false) MultipartFile attach){
+        try {
+            request.setCharacterEncoding( "utf-8" );
+            response.setHeader( "Content-Type" , "text/html" );
+            String rootPath = request.getSession().getServletContext().getRealPath("/resources/upload/");
+
+            /**
+             * 文件路径不存在则需要创建文件路径
+             */
+            File filePath=new File(rootPath);
+            if(!filePath.exists()){
+                filePath.mkdirs();
+            }
+
+            //最终文件名
+            File realFile=new File(rootPath+File.separator+attach.getOriginalFilename());
+            FileUtils.copyInputStreamToFile(attach.getInputStream(), realFile);
+
+            //下面response返回的json格式是editor.md所限制的，规范输出就OK
+            response.getWriter().write( "{\"success\": 1, \"message\":\"上传成功\",\"url\":\"/resources/upload/" + attach.getOriginalFilename() + "\"}" );
+        } catch (Exception e) {
+            try {
+                response.getWriter().write( "{\"success\":0}" );
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+        }
     }
 
 
