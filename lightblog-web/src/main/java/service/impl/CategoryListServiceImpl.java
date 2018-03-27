e74a02577967f456c52b39b6ce1c2268e820e1c8
cym <cympr@outlook.com>
2018-03-27 20:10:18 +0800

    fixed {catagoryList page - category be unique }

--- lightblog-web/src/main/java/service/impl/CategoryListServiceImpl.java
+++ lightblog-web/src/main/java/service/impl/CategoryListServiceImpl.java
@@ -30,7 +30,7 @@ public class CategoryListServiceImpl implements CategoryListService {
         log.info("---获取分类Map处理：start");
         Map<String, Object> resultMap = new HashMap<String,Object>();
         List<Map<String, Object>> categoryList = new ArrayList<Map<String, Object>>();//存放分类map列表
-        Map<String, Object> categoryListItemMap = new HashMap<String, Object>();//存放分类信息：分类名称+权值
+        Map<String, Object> categoryMap = new HashMap<String, Object>();//存放分类信息：分类名称+权值
         String categoryName = "";
         String category;
         int count = 0;
@@ -41,26 +41,27 @@ public class CategoryListServiceImpl implements CategoryListService {
         WorkInfoExample workInfoExample = (WorkInfoExample)applicationContext.getBean("workInfoExample");
         workInfoExample.or().andWorkUserIdEqualTo(ownerId);
         List<WorkInfo> workInfoList = workInfoMapper.selectByExample(workInfoExample);
+        //分类统计
         for (int i = 0; i < workInfoList.size(); i++) {
             category = workInfoList.get(i).getWorkCategory();
-            if (i==0){
-                categoryName = category;
-                categoryListItemMap.put("categoryName",categoryName);categorySum++;
-                count++;
-            }else if (categoryName.equals(category)){
-                count++;
+            if (categoryMap.get(category)==null){
+                log.info("遍历得到了新分类："+ category);
+                categoryMap.put(category,1);
             }else {
-                categoryListItemMap.put("sum",count);
-                categoryList.add(categoryListItemMap);
-                categoryListItemMap = new HashMap<String, Object>();
-                categoryName = category;
-                categoryListItemMap.put("categoryName",categoryName);categorySum++;
-                count = 0;
+                count = Integer.parseInt(categoryMap.get(category).toString());
+                categoryMap.put(category,++count);
             }
         }
-        categoryListItemMap.put("sum",count);
-        categoryList.add(categoryListItemMap);
-
+        //按格式封装分类信息
+        log.info("遍历分类集合：");
+        for (String key:categoryMap.keySet()){
+            Map<String, Object> categoryListItemMap = new HashMap<String, Object>();//按格式封装分类信息：分类名称+权值
+            categoryListItemMap.put("categoryName",key);
+            categoryListItemMap.put("sum",categoryMap.get(key));
+            categoryList.add(categoryListItemMap);
+            categorySum++;
+        }
+        log.info("打印categoryList： "+categoryList.toString());
         resultMap.put("categorySum",categorySum);
         resultMap.put("categoryList",categoryList);
         log.info("---获取分类Map处理：end");
